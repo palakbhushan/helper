62f4e139a91db66432165d03f1b24967eac87e73
Palak Bhushan <palakbosw@gmail.com>
2022-08-19 12:17:51 +0530

    adding initial file

---
+++ index.js
@@ -0,0 +1,7 @@
+var http = require('http');
+var crypto = require('crypto');
+//create a server object:
+http.createServer(function (req, res) {
+  res.write('HELLO WORLD and hello universe'); //write a response to the client
+  res.end(); //end the response
+}).listen(8080); //the server object listens on port 808
